e611ed0a46f6f4f390e7d59c3349ebd9f931b11e
MaganaAsiati <assia.magana@gmail.com>
2022-09-02 14:46:06 +0200

    add validation for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id', dependent: :destroy
   has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :user_id, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 1, only_integer: true }
 end
