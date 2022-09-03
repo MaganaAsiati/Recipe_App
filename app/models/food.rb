01b0e0f363cd5bb0eca45948c9b0628b0de675e7
MaganaAsiati <assia.magana@gmail.com>
2022-09-03 02:40:14 +0200

    add pictures

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id', dependent: :destroy
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
   validates :name, presence: true
   validates :measurement_unit, presence: true
